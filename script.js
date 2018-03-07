2328cbecd8926a454ce353e5958c5e3a35fc28f9
inewham <israel.newham@saiyansolutions.com>
2018-03-07 21:20:13 +0000

    Added Next and Previous arrows to main image modals

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 $('#exampleModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 })
+
+
+
+$("div[id^='myImg']").each(function(){
+  
+  var currentModal = $(this);
+  
+  //click next
+  currentModal.find('.carousel-control-next').click(function(){
+    currentModal.modal('hide');
+    currentModal.closest("div[id^='myImg']").nextAll("div[id^='myImg']").first().modal('show'); 
+  });
+  
+  //click prev
+  currentModal.find('.carousel-control-prev').click(function(){
+    currentModal.modal('hide');
+    currentModal.closest("div[id^='myImg']").prevAll("div[id^='myImg']").first().modal('show'); 
+  });
+
+});
